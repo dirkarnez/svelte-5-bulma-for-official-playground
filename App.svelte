672a1bec84ceeed58cb12ac17cd469c80700fc13
Dirk Arnez <smalldirkalex@gmail.com>
2024-12-27 18:28:39 +0800

    - add App.svelte

---
+++ App.svelte
@@ -0,0 +1,16 @@
+<svelte:head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css">
+</svelte:head>
+
+<section class="section">
+	<div class="container">
+		<h1 class="title">
+			Hello World
+		</h1>
+		<p class="subtitle">
+			My first website with <strong>Bulma</strong>!
+		</p>
+	</div>
+</section>
